bd3ee343e43d5129e00836af49fb464b682b10e3
ECS <ECS@DESKTOP-048HU3O>
2022-12-20 11:43:58 +0900

    [github Project] spring boot 초기셋팅 autor: ksmart45 이유민

---
+++ gitTest/src/main/java/ksmart/mybatis/GitTestApplication.java
@@ -0,0 +1,13 @@
+package ksmart.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestApplication.class, args);
+	}
+
+}
---
+++ gitTest/src/test/java/ksmart/mybatis/GitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
